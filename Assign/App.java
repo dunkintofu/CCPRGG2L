99e4882287f04d50bc0ed120b3761a3fe4e41f6b
Marielle Kyla E. Villena <111546442+dunkintofu@users.noreply.github.com>
2023-01-30 14:24:32 +0800

    Create App.java

---
+++ Assign/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Character Class
+        Gun classic = new Gun(12, "Classic", 6.75 );
+        classic.sayGunName();
+        classic.sayMagazineCapacity();
+        classic.sayFireRate();
+
+        Gun phantom = new Gun(30, "Phantom", 5.4);
+        phantom.sayGunName();
+        phantom.sayMagazineCapacity();
+        phantom.sayFireRate();
+
+        Gun operator = new Gun(5, "Operator", 0.6);
+        operator.sayGunName();
+        operator.sayMagazineCapacity();
+        operator.sayFireRate();
+
+    }
+}
