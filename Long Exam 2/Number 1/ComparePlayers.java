278fb19721322ef3961f00c367d7dda526f07f2b
Marielle Kyla E. Villena <111546442+dunkintofu@users.noreply.github.com>
2023-03-05 11:43:30 +0800

    Create ComparePlayers.java

---
+++ Long Exam 2/Number 1/ComparePlayers.java
@@ -0,0 +1,37 @@
+
+public class ComparePlayers extends Player {
+ComparePlayers(String name, String team, int jerseyNumber) {
+        super(name , team, jerseyNumber);
+    }
+
+    public static void main(String[] args) {
+
+    
+    Player player1 = new Player("", "", 0);
+    Player player2 = new Player("", "", 0);
+    
+    //read player1 info
+    System.out.println("Enter player 1 information:");
+    player1.readPlayer();
+    System.out.println("Player 1 info: " + player1.name + ", " + player1.team + ", " + player1.jerseyNumber);
+
+    //read player2 info
+    System.out.println("Enter player 2 information:");
+    player2.readPlayer();
+    System.out.println("Player 2 info: " + player2.name + ", " + player2.team + ", " + player2.jerseyNumber);
+
+    //call equals method
+    if (player1.team.equals(player2.team) && player1.jerseyNumber == player2.jerseyNumber) {
+        System.out.println("true");
+    } else {
+        System.out.println("false");
+    }
+    }
+
+    // public static boolean equals(Object o){
+    //     if(this == 0)
+    //     return false;
+
+    // }
+
+}
