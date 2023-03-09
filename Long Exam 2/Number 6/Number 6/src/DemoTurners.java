bf957d297e6bd6723e7a6c51386c83b9d299f7c6
Marielle Kyla E. Villena <111546442+dunkintofu@users.noreply.github.com>
2023-03-09 12:12:00 +0800

    Add files via upload

---
+++ Long Exam 2/Number 6/Number 6/src/DemoTurners.java
@@ -0,0 +1,13 @@
+public class DemoTurners {
+    public static void main(String[] args) {
+        Leaf Leaf = new Leaf();
+        Leaf.turn();
+
+        Page Page = new Page();
+        Page.turn();
+
+        Pancake Pancake = new Pancake();
+        Pancake.turn();
+    }
+    
+}
